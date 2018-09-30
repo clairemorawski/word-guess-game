ce6f322c6acdd94dffa79ff5c2dc3f1229304d6c
That_Guy <eghove@gmail.com>
2018-09-30 12:26:55 -0500

    Started on some javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+const words = ["Summit", "Surly", "Schells"];
+let maxWords = words.length;
+let wins = 0;
+
+//this function iterates wins variable
+function wonGame() {
+    wins++;
+}
+
+//the random number generator
+function getRandomInt() {
+    return Math.floor(Math.random() * Math.floor(max));
+    }
+
