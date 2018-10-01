19be1978ba3cbba7612890afc71d78c21fb06ac8
That_Guy <eghove@gmail.com>
2018-10-01 16:19:55 -0500

    built some more component functions, wordSelector and wordSplitter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,12 @@
-const words = ["Summit", "Surly", "Schells"];
+//GLOBAL VARIABLES----------------------------------------------
+
+const words = ["Summit", "Surly", "Schells"]; // the word bank array
 let maxWords = words.length;
 let wins = 0;
+let wordSelected="";
+let lettersGuessed=[];
+let letterToCheck;
+let numberOfGuesses=5;
 
 //this function iterates wins variable
 function wonGame() {
@@ -8,15 +14,10 @@ function wonGame() {
 }
 
 //the random number generator
-function getRandomInt() {
+function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
-    }
+}
 
-let lettersGuessed=[];
-
-let letterToCheck;
-
-let numberOfGuesses=5;
 
 //to check the letter guessed against those already guessed
 
@@ -46,7 +47,7 @@ function keepPlaying() {
     }
 }
 
-//Reset Game functuin
+//Reset Game function
 function resetGame() {
     lettersGuessed=[]; // reset letters guessed
     printLettersGuessed();
@@ -54,8 +55,33 @@ function resetGame() {
     printGuesses()
 }
 
-// GAMEPLAY
+//word selector function
+function wordSelector() {
+    let randomNumber=0; //creates a local variable randomNumber
+    randomNumber=getRandomInt(maxWords); //sets randomNumber equal to getRandomInt call
+    wordSelected=words[randomNumber]; // sets global variable to random index value of words array
+    console.log(wordSelected); // for debugging
+}
+
+//word splitter function, splits the selected word apart
+function wordSplitter(value) {
+    let splitWord=[]; //creates an empty array, maybe move this to globabl?
+    splitWord=value.split(""); // splits the string apart into its constituents into the splitWord array
+    console.log(splitWord); //debugging
+    console.log(splitWord.length); //debugging
+}
+
+//trying to build the guesser component
+function guesser(arr) {
+
+}
+
+// ---------------------GAMEPLAY---------------------------------------------
+wordSelector();
+wordSplitter(wordSelected);
 printGuesses();
+
+
 
 document.onkeyup = function(event) {
     letterToCheck = (event.key); //set letterToCheck to the letter typed on keyboard
